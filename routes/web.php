166c34142c596d00d33549bfa1778438efae624a
Andy Moreno <andymoreno.ing@gmail.com>
2021-07-04 18:37:52 -0500

    mostrando posts segun tags

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 Route::get('/',[PostController::class,'index'])->name('posts.index');
 Route::get('posts/{post}',[PostController::class,'show'])->name('posts.show');
 Route::get('category/{category}',[PostController::class,'category'])->name('posts.category');
+Route::get('tag/{tag}',[PostController::class,'tag'])->name('posts.tag');
